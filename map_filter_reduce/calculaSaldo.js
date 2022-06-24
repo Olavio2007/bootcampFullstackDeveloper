6129530abd0042f988d98a46f33f2f32d0cda648
Olavio2007 <103150473+Olavio2007@users.noreply.github.com>
2022-06-24 18:42:41 -0300

    Add files via upload

---
+++ map_filter_reduce/calculaSaldo.js
@@ -0,0 +1,57 @@
+const lista = [
+    {
+        name: 'sabao em po',
+        preco: 30
+    },
+    {
+        name: 'cereal',
+        preco: 12
+    },
+    {
+        name: 'toalha',
+        preco: 30
+    },
+];
+
+const saldoDisponivel = 100;
+
+function calculaSaldo(saldoDisponivel, lista) {
+    return lista.reduce(function (prev, current, index){
+        console.log('rodada', index + 1);
+        console.log({prev});
+        console.log({current});
+        return prev - current.preco;
+
+    }, saldoDisponivel);
+}
+
+console.log(calculaSaldo(saldoDisponivel, lista));
+
+/*
+function calculaSaldo(saldo, itens) {
+	if (!saldo || !itens || !itens.length) return 'Envie todos os parâmetros';
+
+	const saldoFinal = itens.reduce((acc, item) => acc - item.preco, saldo);
+
+	return `O saldo final será de ${saldoFinal} reais`;
+}
+
+lista = [
+	{
+		preco: 2,
+		nome: 'maçã',
+	},
+	{
+		preco: 30,
+		nome: 'roupa',
+	},
+	{
+		preco: 25,
+		nome: 'carne',
+	},
+];
+
+saldo = 100;
+
+console.log(calculaSaldo(saldo, lista));
+*/
