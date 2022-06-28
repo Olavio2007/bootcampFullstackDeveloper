71d236704ee62e2a28f77f244c3fe6d68f925542
Olavio2007 <103150473+Olavio2007@users.noreply.github.com>
2022-06-28 18:53:44 -0300

    Add files via upload

---
+++ modulos/atividades/main.mjs
@@ -0,0 +1,7 @@
+import { mostraIdade, mostraCidade, mostraHobby } from './funcoes.mjs';
+
+console.log(mostraIdade('Olavio', 44));
+
+console.log(mostraCidade('Olavio', 'Brasília'));
+
+console.log(mostraHobby('Olavio', 'Futebol'));
